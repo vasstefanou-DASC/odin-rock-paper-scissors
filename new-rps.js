464b0863ba93582ddf88f42ba7fbd85573cd00d5
Vasileios Stefanou <vas.stefanou@gmail.com>
2026-03-08 16:31:50 +0200

    Add human,computer choice and play round

--- new-rps.js
+++ new-rps.js
@@ -14,7 +14,33 @@ function getComputerChoice() {
         case 2:
             computerChoice = "paper";
     }
-    return computerChoice;
+    return {value: randomNumber,choice: computerChoice};
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    const humanChoice = prompt("What is your pick? (rock,scissors,paper)").toLowerCase();
+    let humanValue = -1;
+    if (humanChoice==="rock"){
+        humanValue = 0;
+    } else if (humanChoice==="scissors") {
+        humanValue = 1;
+    } else if (humanChoice==="paper") {
+        humanValue = 2;
+    }
+    return {value: humanValue,choice: humanChoice};
+}
+
+const humanScore = 0;
+const computerScore = 0;
+
+function playRound (human,computer) {
+    if (human.value-computer.value === -1 || human.value-computer.value === 2) {
+        console.log("You win " + human.choice + " beats " + computer.choice);
+    } else if (human.value-computer.value === -2 || human.value-computer.value === 1) {
+        console.log("You lose " + human.choice + " loses to " + computer.choice)
+    } else {
+        console.log("It's a Draw");
+    }
+}
+
+playRound(getHumanChoice(),getComputerChoice());
